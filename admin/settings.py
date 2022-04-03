6a1f242239c631cac9e77dc0c8eb7d316e2dc611
Gabriele Frattini <gabriele.g.frattini@gmail.com>
2022-04-04 00:08:23 +0200

    dev mode

--- admin/settings.py
+++ admin/settings.py
@@ -31,7 +31,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 #dev
 
 ALLOWED_HOSTS = ["cv-match.herokuapp.com", "127.0.0.1"]
