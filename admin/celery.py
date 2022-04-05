199235997e331f302468f1e9da5612a8fd24aec6
Gabriele Frattini <gabriele.g.frattini@gmail.com>
2022-04-05 13:23:21 +0200

    fixed celery worker bug

--- admin/celery.py
+++ admin/celery.py
@@ -5,7 +5,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin.settings')
 
 app = Celery('admin')
 
-app = Celery('tasks', backend='rpc://', broker='pyamqp://')
-
 app.autodiscover_tasks()
 
